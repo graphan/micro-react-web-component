b0bb2d3fcdf1f654b36ffa792482c1eff8202a18
Tomasz Adrianowski <tomasz.adrianowski@metro-external.digital>
2024-04-16 15:32:50 +0200

    Push App build to the repo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MyComponent from './MyComponent';
+import App from './App';
 import { defineCustomElement } from '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 
 class MyCustomElement extends HTMLElement {
   connectedCallback() {
-    ReactDOM.render(<MyComponent />, this);
+    ReactDOM.render(<App />, this);
   }
   disconnectedCallback() {
     ReactDOM.unmountComponentAtNode(this);
